767d74453a835424887ec92ba4dfad627dbc69ae
Виктор Андрюшенков <buktop@MacBook-Air-Viktor.local>
2018-02-15 21:49:01 +0900

    First release

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Coursera Dump
+The script collects information about the different courses on the Courser, leads it into a convenient view, and uploads it to a file in the Exel format.
+# Get started:
 
-[TODO. There will be project description]
+An example of running a script in Linux, Python 3.5:
+
+```bash
+$ python3 coursera.py
+https://www.coursera.org/learn/gamification:
+					name: Gamification
+					language: English, Subtitles: Ukrainian, Chinese (Simplified), Portuguese (Brazilian), Vietnamese, Russian, Turkish, Spanish, Kazakh
+					begin date: Feb 26
+					rating: no data
+https://www.coursera.org/learn/missing-data:
+					name: Dealing With Missing Data
+					language: English
+					begin date: Feb 19
+					rating: 3.9
+https://www.coursera.org/learn/vital-signs:
+					name: Vital Signs: Understanding What the Body Is Telling Us
+					language: English, Subtitles: Dutch
+					begin date: Feb 19
+					rating: no data
+https://www.coursera.org/learn/modern-art-ideas:
+					name: Modern Art & Ideas
+					language: English, Subtitles: Italian
+					begin date: Feb 19
+					rating: 4.6
+```
+
+Information about the courses will be saved in the file named coursera.xlsx in the project directory.
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -1,16 +1,89 @@
+import requests
+from openpyxl import Workbook
+from lxml import etree
+from bs4 import BeautifulSoup
+import re
 
 
-def get_courses_list():
-    pass
+def get_courses_list(
+        top_size,
+        url='https://www.coursera.org/sitemap~www~courses.xml'
+):
+    try:
+        response = requests.get(url)
+        if response.ok:
+            text = response.content
+            xml = etree.fromstring(text)
+            url_courses_list = []
+            for url in xml.getchildren()[:top_size]:
+                for loc in url.getchildren():
+                    text = loc.text
+                    url_courses_list.append(text)
+            return url_courses_list
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        return None
 
 
 def get_course_info(course_slug):
-    pass
+    try:
+        response = requests.get(course_slug)
+        if response.ok:
+            parse = BeautifulSoup(response.content, 'html.parser')
+            name_course = parse.find("h1").text
+            language_course = parse.find('div', {'class': 'rc-Language'})
+            if language_course:
+                language_course = language_course.text
+            else:
+                language_course = "no data"
+            begin_date_course = parse.find('div', {'class': 'startdate rc-StartDateString caption-text'}).text
+            if begin_date_course == 'No Upcoming Session Available':
+                begin_date_course = 'No Session'
+            else:
+                begin_date_course = " ".join(begin_date_course.split()[1:])
+            avarage_rating = parse.find(
+                'div',
+                {'class': 'ratings-text bt3-hidden-xs'}
+            )
+            if avarage_rating:
+                avarage_rating = re.findall(r'\d\.\d', avarage_rating.text)[0]
+            else:
+                avarage_rating = "no data"
+            course_inf = {
+                'name': name_course,
+                'language': language_course,
+                'begin date': begin_date_course,
+                'rating': avarage_rating
+            }
+            return course_inf
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        return None
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_courses_info_to_xlsx(file_name, top_size):
+    url_courses_list = get_courses_list(top_size)
+    if url_courses_list:
+        work_book = Workbook()
+        work_sheet = work_book.active
+        for url_courses in url_courses_list:
+            course_info = get_course_info(url_courses)
+            if course_info:
+                work_sheet.append(list(course_info.values()))
+                print("{url}:".format(url=url_courses))
+                for key, value in course_info.items():
+                    print('\t\t\t\t\t{key}: {value}'.format(
+                        key=key,
+                        value=value
+                    ))
+            else:
+                print ('{} - can not connect.'.format(url_courses))
+                continue
+        work_book.save(filename=file_name)
+        print ('Information is written to a file {}'.format(file_name))
+        return True
 
 
 if __name__ == '__main__':
-    pass
+    top_size = 20
+    file_name = "coursera.xlsx"
+    if output_courses_info_to_xlsx(file_name, top_size) is None:
+        print("Can not connect!")
