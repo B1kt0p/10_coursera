40d63b7be92105fe990b1b464596dd7444ce56a9
Виктор Андрюшенков <buktop@MacBook-Air-Viktor.local>
2018-02-25 21:45:45 +0900

    Fixed the names of variables and functions, separated processing of data from their input

--- README.md
+++ README.md
@@ -6,24 +6,7 @@ An example of running a script in Linux, Python 3.5:
 
 ```bash
 $ python3 coursera.py -n coursera.xlsx
-https://www.coursera.org/learn/solid-waste-management:
-					name: Municipal Solid Waste Management in Developing Countries
-					language: English, Subtitles: Spanish, Indonesian, French
-					begin date: Feb 05
-					rating: 4.8
-					url: https://www.coursera.org/learn/solid-waste-management
-https://www.coursera.org/learn/music-and-social-action:
-					name: Music and Social Action
-					language: English
-					begin date: Mar 05
-					rating: 4.5
-					url: https://www.coursera.org/learn/music-and-social-action
-https://www.coursera.org/learn/protools:
-					name: Pro Tools Basics
-					language: English
-					begin date: Mar 12
-					rating: 4.7
-					url: https://www.coursera.org/learn/protools
+File coursera.xlsx saved.
 ```
 
 Information about the courses will be saved in the file named coursera.xlsx in the project directory.
--- coursera.py
+++ coursera.py
@@ -19,49 +19,46 @@ def get_argv():
     return parser.parse_args()
 
 
-def get_courses_list(
+def get_courses_url_list(
         top_size,
-        url='https://www.coursera.org/sitemap~www~courses.xml'
+        xml_data
 ):
-    response = requests.get(url)
-    if response.ok:
-        text = response.content
-        xml = etree.fromstring(text)
-        url_courses_list = []
-        for url in xml.getchildren()[:top_size]:
-            for loc in url.getchildren():
-                text = loc.text
-                url_courses_list.append(text)
-        return url_courses_list
+    xml = etree.fromstring(xml_data)
+    url_courses_list = []
+    for url in xml.getchildren()[:top_size]:
+        for loc in url.getchildren():
+            text = loc.text
+            url_courses_list.append(text)
+    return url_courses_list
 
 
-def get_html(url):
+def fetch_content(url):
     response = requests.get(url)
     if response.ok:
         return response.content
 
 
 def get_course_info(page_course_html, url_course):
-    parse = BeautifulSoup(page_course_html, 'html.parser')
-    name_course = parse.find("h1").text
-    language_course = parse.find('div', {'class': 'rc-Language'})
+    soup = BeautifulSoup(page_course_html, 'html.parser')
+    name_course = soup.find("h1").text
+    language_course = soup.find('div', {'class': 'rc-Language'})
     if language_course:
         language_course = language_course.text
     else:
-        language_course = "no data"
-    begin_date_course = parse.find('div', {'class': 'startdate rc-StartDateString caption-text'}).text
+        language_course = None
+    begin_date_course = soup.find('div', {'class': 'startdate rc-StartDateString caption-text'}).text
     if begin_date_course == 'No Upcoming Session Available':
-        begin_date_course = 'No Session'
+        begin_date_course = None
     else:
         begin_date_course = " ".join(begin_date_course.split()[1:])
-    avarage_rating = parse.find(
+    avarage_rating = soup.find(
         'div',
         {'class': 'ratings-text bt3-hidden-xs'}
     )
     if avarage_rating:
         avarage_rating = re.findall(r'\d\.\d', avarage_rating.text)[0]
     else:
-        avarage_rating = "no data"
+        avarage_rating = None
     course_info = {
         'name': name_course,
         'language': language_course,
@@ -72,7 +69,7 @@ def get_course_info(page_course_html, url_course):
     return course_info
 
 
-def create_xlsx_file():
+def create_work_sheet():
     work_book = Workbook()
     work_sheet = work_book.active
     title = [
@@ -86,42 +83,34 @@ def create_xlsx_file():
     return work_book
 
 
-def add_courses_info_to_xlsx(work_book, course_info):
+def add_courses_info_to_work_sheet(work_book, course_info):
     work_sheet = work_book.active
-    work_sheet.append([
-        course_info['name'],
-        course_info['language'],
-        course_info['begin date'],
-        course_info['rating'],
-        course_info['url']
-    ])
+    added_row = ['no data' if course_info[key] is None else course_info[key] for key in course_info]
+    work_sheet.append(added_row)
     return work_book
 
 
 def save_xlsx_file(work_book, file_name):
-    work_book.save(filename=file_name)
-
-
-def print_course_info(course_info):
-    print("{url}:".format(url=course_info['url']))
-    for key, value in course_info.items():
-        print('\t\t\t\t\t{key}: {value}'.format(key=key, value=value))
+    work_book.save(file_name)
 
 
 if __name__ == '__main__':
     file_name = get_argv().name
     top_size = 20
-    work_book = create_xlsx_file()
+    url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    work_book = create_work_sheet()
     try:
-        courses_list = get_courses_list(top_size)
-        for url_course in courses_list:
-            page_course_html = get_html(url_course)
-            if page_course_html:
-                course_info = get_course_info(page_course_html, url_course)
-                add_courses_info_to_xlsx(work_book, course_info)
-                print_course_info(course_info)
-            else:
-                continue
-        save_xlsx_file(work_book, file_name)
+        xml_data = fetch_content(url)
+        if xml_data:
+            courses_url_list = get_courses_url_list(top_size, xml_data)
+            for url_course in courses_url_list:
+                page_course_html = fetch_content(url_course)
+                if page_course_html:
+                    course_info = get_course_info(page_course_html, url_course)
+                    add_courses_info_to_work_sheet(work_book, course_info)
+            save_xlsx_file(work_book, file_name)
+            print('File {} saved.'.format(file_name))
+        else:
+            print('Can not connect!')
     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-        print("Can not connect!")
+        print('Can not connect!')
